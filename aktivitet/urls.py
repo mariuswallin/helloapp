e395d246d460a01a88329804eda5b77cff880a89
Marius Johansen <post@mariuswallin.no>
2016-05-01 22:33:29 +0200

    Added pages / TemplateView

--- aktivitet/urls.py
+++ aktivitet/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from collection import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
