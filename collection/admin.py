155ab33e9a627178f4e37ba7eada3a4726e7e9bb
Marius Johansen <post@mariuswallin.no>
2016-05-03 22:26:13 +0200

    Made the first model

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from collection.models import Aktiviteter
+
+class AktivitetAdmin(admin.ModelAdmin):
+    model = Aktiviteter
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Aktiviteter,AktivitetAdmin)
+
+#legger name automatisk til slug.
--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Aktiviteter(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Aktivitet'
+        verbose_name_plural = 'Aktiviteter'
+
+
