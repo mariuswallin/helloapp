9589762a29741ab6cfabe0458f3c7a24750fd62c
Marius Johansen <post@mariuswallin.no>
2016-05-16 13:47:39 +0200

    before heroku

--- aktivitet/settings.py
+++ aktivitet/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'aktivitet/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
