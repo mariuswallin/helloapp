a86978bc9f73db903282d4db0c845dec3c31e409
Marius Johansen <post@mariuswallin.no>
2016-05-16 13:17:20 +0200

    Commiting before pushing to Heroku Fire

--- aktivitet/settings.py
+++ aktivitet/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Due to docs django-reduction-redux - how many days it can go before you ahve to activate your account
 
